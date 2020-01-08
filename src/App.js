b9d5ac3c0166412e8f0d8f357afbe52f485d6c40
Bay Max <baymax@Bays-iMac.local>
2020-01-08 13:56:26 -0500

    add theme context and applied

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
+import BookList from './components/BookList'
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }

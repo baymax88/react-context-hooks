0d04924ac3f055734d22a40508717dd3eb2b8408
Bay Max <smartbay1@outlook.com>
2020-07-30 23:39:03 -0400

    state, effect hook practice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import BookList from './components/BookList'
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './contexts/AuthContext';
+import SongList from './components/SongList'
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <SongList />
     </div>
   );
 }

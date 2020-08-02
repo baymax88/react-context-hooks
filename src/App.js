50d04d0141bb3761d45d6e6e1e5a38b201dc240e
Bay Max <smartbay1@outlook.com>
2020-08-02 04:07:34 -0400

    build booklist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import BookList from './components/BookList'
-import ThemeContextProvider from './contexts/ThemeContext'
-import ThemeToggle from './components/ThemeToggle'
-import AuthContextProvider from './contexts/AuthContext'
 import BookContextProvider from './contexts/BookContext'
+import Navbar from './components/Navbar'
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookContextProvider>
-            <BookList />
-          </BookContextProvider>
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <BookForm />
+      </BookContextProvider>
     </div>
   );
 }
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,20 +1,27 @@
 import React, { createContext, useState } from 'react'
+import { v1 as uuidv1 } from 'uuid'
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
     const [books, setBooks] = useState([
-        {title: 'name of the wind', id: 1},
-        {title: 'the way of kings', id: 2},
-        {title: 'the final empire', id: 3},
-        {title: 'the hero of ages', id: 4},
+        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+        {title: 'the final empire', author: 'brandon sanderson', id: 2},
     ])
 
+    const addBook = (title, author) => {
+        setBooks([...books, {title, author, id: uuidv1()}])
+    }
+
+    const removeBook = (id) => {
+        setBooks(books.filter(book => book.id !== id))
+    }
+
     return (
-        <BookContext.Provider value={{books}}>
+        <BookContext.Provider value={{books, addBook, removeBook}}>
             {props.children}
         </BookContext.Provider>
     )
 }
- 
+
 export default BookContextProvider
